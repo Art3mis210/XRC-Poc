26d6920c88acfa9730703e13f780bc8138c35e49
Art3mis210 <77666553+Art3mis210@users.noreply.github.com>
2022-08-08 21:04:21 +0530

    OPTIMIZED WELDING MODEL

--- Assets/Scripts/Welding/WeldingHelmet.cs
+++ Assets/Scripts/Welding/WeldingHelmet.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Oculus.Interaction.HandPosing;
 
 public class WeldingHelmet : MonoBehaviour
 {
     [SerializeField] Transform PlayerCamera;
-    public void HelmetPicked()
+    bool HelmetPicked;
+    HandGrabInteractable handGrab;
+    private void Start()
     {
-        if(Vector3.Distance(transform.position,PlayerCamera.position)<0.5f)
-            transform.gameObject.SetActive(false);
+        handGrab = GetComponent<HandGrabInteractable>();
+    }
+
+    public void HelmetPick(bool Status)
+    {
+        if(!Status)
+        {
+            if(handGrab.InteractorsCount == 0)
+                HelmetPicked = Status;
+        }
+        else
+            HelmetPicked = Status;
+
+    }
+    private void Update()
+    {
+        if(HelmetPicked)
+        {
+       
+            Debug.Log(Vector3.Distance(transform.position, PlayerCamera.position));
+            if (Vector3.Distance(transform.position, PlayerCamera.position) < 0.2f)
+                transform.gameObject.SetActive(false);
+        }
     }
 }
