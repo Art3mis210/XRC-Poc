06c63c4d007665b217e4ec16295127d54fcb11d3
Nitish-K15 <79478893+Nitish-K15@users.noreply.github.com>
2022-08-08 23:38:34 +0530

    Merge branch 'main' of https://github.com/Art3mis210/XRC-Poc

--- Assets/Scripts/Welding/WeldingHelmet.cs
+++ Assets/Scripts/Welding/WeldingHelmet.cs
@@ -1,12 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Oculus.Interaction.HandPosing;
 
 public class WeldingHelmet : MonoBehaviour
 {
-    public void HelmetPicked()
+    [SerializeField] Transform PlayerCamera;
+    bool HelmetPicked;
+    HandGrabInteractable handGrab;
+    private void Start()
     {
-        //Helmet Picked
-        transform.gameObject.SetActive(false);
+        handGrab = GetComponent<HandGrabInteractable>();
+    }
+
+    public void HelmetPick(bool Status)
+    {
+        if(!Status)
+        {
+            if(handGrab.InteractorsCount == 0)
+                HelmetPicked = Status;
+        }
+        else
+            HelmetPicked = Status;
+
+    }
+    private void Update()
+    {
+        if(HelmetPicked)
+        {
+       
+            Debug.Log(Vector3.Distance(transform.position, PlayerCamera.position));
+            if (Vector3.Distance(transform.position, PlayerCamera.position) < 0.2f)
+                transform.gameObject.SetActive(false);
+        }
     }
 }
